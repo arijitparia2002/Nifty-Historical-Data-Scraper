d03e069a2b7dfd9d901e1e87292708cf0e4de51a
Arijit Paria <arijitparia2020@gmail.com>
2023-11-08 15:26:41 +0530

    implemented all the scrapping function

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
 from utils.scraper import scrape_data
+from datetime import datetime, timedelta
+import time
 
 def main():
     # Initialize the web driver
     driver_path = 'drivers/chromedriver'
     driver = webdriver.Chrome(executable_path=driver_path)
+
+    # Get the current date
+    current_date = datetime.now()
+
+    # Calculate the date one year ago from the current date
+    one_year_ago = current_date - timedelta(days=365)
+
+    # Convert the dates to Unix timestamps
+    end_date_timestamp = int(time.mktime(current_date.timetuple()))
+    start_date_timestamp = int(time.mktime(one_year_ago.timetuple()))
     
     # URL to be scraped
-    url = 'https://in.investing.com/indices/s-p-cnx-nifty-historical-data'
+    # url = 'https://in.investing.com/indices/s-p-cnx-nifty-historical-data'
+    url = f"https://in.investing.com/indices/s-p-cnx-nifty-historical-data?end_date={end_date_timestamp}&st_date={start_date_timestamp}"
     
     # Perform the scraping
     scrape_data(driver, url)
--- utils/scraper.py
+++ utils/scraper.py
@@ -1,27 +1,52 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+from selenium import webdriver
 
-def scrape_data(driver, url):
-    # Open the website
-    driver.get(url)
-    
-    # Example of how to interact with the website:
-    # Wait for the date dropdown to be clickable and then click it
-    date_dropdown = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, 'the_id_of_date_dropdown'))
+# Function to scrape data and save to an Excel file
+def scrape_to_excel(driver, table_selector, excel_filename):
+    WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, table_selector))
     )
-    date_dropdown.click()
+    table = driver.find_element(By.CSS_SELECTOR, table_selector)
 
-    # Change the date to one year (this would be specific to the page's structure)
-    # You would need to find the correct selectors for the date options
-    # ...
 
-    # Scrape the monthly, weekly, daily data
-    # This is also specific to how the data is presented on the page
-    # ...
+    headers = [header.text for header in table.find_elements(By.TAG_NAME, "th")]
 
-    # Save the scraped data to a file in the 'data/' folder
-    # ...
 
-    print('Data scraping is complete.')
+    data = [
+        [cell.text for cell in row.find_elements(By.TAG_NAME, "td")]
+        for row in table.find_elements(By.TAG_NAME, "tr")
+        if row.find_elements(By.TAG_NAME, "td")  # Exclude rows without data cells
+    ]
+    df = pd.DataFrame(data, columns=headers)
+    df.to_excel(excel_filename, index=False)
+    print(f'Data saved to {excel_filename}')
+
+# Function to click on the navigation tab
+def click_nav_tab(driver, nav_tab_selector):
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, nav_tab_selector))
+    ).click()
+
+# function to scrape data
+def scrape_data(driver, url):
+    try:
+        # Open the website
+        driver.get(url)
+        
+        # Scrape daily data
+        scrape_to_excel(driver, "#js-main-container > section.main-container.container > div > section.instrument.js-section-content > section.js-table-wrapper.common-table-comp.scroll-view > div.common-table-wrapper > div > table", 'data/daily.xlsx')
+        
+        # Click the weekly tab and scrape weekly data
+        click_nav_tab(driver, "#js-main-container > section.main-container.container > div > section.instrument.js-section-content > section.js-table-wrapper.common-table-comp.scroll-view > section.table-nav.common-section > div.start-side > nav > ul > li:nth-child(2)")
+        scrape_to_excel(driver, "#js-main-container > section.main-container.container > div > section.instrument.js-section-content > section.js-table-wrapper.common-table-comp.scroll-view > div.common-table-wrapper > div > table", 'data/weekly.xlsx')
+        
+        # Click the monthly tab and scrape monthly data
+        click_nav_tab(driver, "#js-main-container > section.main-container.container > div > section.instrument.js-section-content > section.js-table-wrapper.common-table-comp.scroll-view > section.table-nav.common-section > div.start-side > nav > ul > li:nth-child(3)")
+        scrape_to_excel(driver, "#js-main-container > section.main-container.container > div > section.instrument.js-section-content > section.js-table-wrapper.common-table-comp.scroll-view > div.common-table-wrapper > div > table", 'data/monthly.xlsx')
+
+    finally:
+        # Close the driver
+        driver.quit()
